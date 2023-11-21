a7ca05905c73044a7dfba940b7aa74545e5003a2
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2023-11-21 14:56:31 -0300

    Scren chat usando rotas de navegação

--- components/chat/chatScreen.tsx
+++ components/chat/chatScreen.tsx
@@ -1,12 +1,19 @@
 import styled from "styled-components/native";
+import Constants from "expo-constants";
+import Icon from "react-native-vector-icons/Ionicons";
+import { Navigation } from "../../utils/interfaces";
+import { NavChat } from "./nav";
 
-export default function ChatScreen() {
+export default function ChatScreen(props: Navigation) {
     return (
-        <Container/>
+        <Container>
+            <NavChat navigation={props}/>
+        </Container>
     )
 }
 
 const Container = styled.SafeAreaView`
     flex: 1;
     background-color: #303030;
+    margin-top: ${Constants.statusBarHeight}px;
 `
--- components/chat/nav.tsx
+++ components/chat/nav.tsx
@@ -1,5 +1,6 @@
 import Icon from "react-native-vector-icons/Ionicons"
 import styled from "styled-components/native"
+import { Navigation } from "../../utils/interfaces"
 
 export default function Nav() {
     return (
@@ -11,6 +12,19 @@ export default function Nav() {
     )
 }
 
+interface Props{
+    navigation: Navigation;
+}
+export function NavChat(props: Props) {
+    return (
+        <NavChatContainer>
+            <ButtonBack onPress={() => props.navigation.navigation.navigate("Chat")}>
+                <Icon name={"arrow-back"} color={"white"} size={30}/>
+            </ButtonBack>
+        </NavChatContainer>
+    )
+}
+
 const Container = styled.View`
     width: 100%;
     height: 50px;
@@ -18,6 +32,21 @@ const Container = styled.View`
 `
 
 const ButtonDrawer = styled.TouchableOpacity`
+    width: 50px;
+    height: 50px;
+    justify-content: center;
+    align-items: center;
+`
+
+const NavChatContainer = styled.View`
+    width: 100%;
+    height: 50px;
+    align-items: center;
+    flex-direction: row;
+    background-color: #10a17d;
+`
+
+const ButtonBack = styled.TouchableOpacity`
     width: 50px;
     height: 50px;
     justify-content: center;
